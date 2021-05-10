083c26998ead1d30c1c5aaea80259a1076ac29fa
Fernando <lf.fernandosantos@gmail.com>
2021-05-10 08:37:13 -0300

    Add tests.

---
+++ working_hours/AddHours.swift
@@ -0,0 +1,15 @@
+//
+//  AddHours.swift
+//  working_hours
+//
+//  Created by Luiz Fernando dos Santos on 10/05/21.
+//
+
+import Foundation
+
+
+class AddHours {
+    func add(_ hour: String) -> Bool {
+        return !hour.isEmpty
+    }
+}
